e77dbff92f4cfe403cfcf84c0c7a74a453b843e1
sergeybiophys <roven2hati@gmail.com>
2021-10-06 10:35:21 +0300

    try2

--- Homework_L1/Homework_L1/MainWindow.xaml.cs
+++ Homework_L1/Homework_L1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homework_L1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,85 +24,75 @@ namespace Homework_L1
     public partial class MainWindow : Window
     {
 
-        Thread t1;
-        Thread t2;
-        Thread t3;
-        Thread t4;
-        Thread t5;
+        //Thread t1;
+        //Thread t2;
+        //Thread t3;
+        //Thread t4;
+        //Thread t5;
 
 
-        TranslateTransform transform1 = new TranslateTransform();
-        TranslateTransform transform2;
-        TranslateTransform transform3;
-        TranslateTransform transform4;
-        TranslateTransform transform5;
+        //TranslateTransform transform1 = new TranslateTransform();
+        //TranslateTransform transform2;
+        //TranslateTransform transform3;
+        //TranslateTransform transform4;
+        //TranslateTransform transform5;
         Random rnd = new Random();
+
+
+        RacerModel racer1;
 
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+           racer1  = new RacerModel(btnRacer1);
+            
+
         }
+
 
         void StartClick(object sender, RoutedEventArgs e)
         {
-            //t1 = new Thread(()=> {
-
-            //    this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate ()
-            //    {
-
-
-            //        while (transform1.X <= 300)
-            //        {
-            //            transform1.X += rnd.Next(5, 45);
-            //            Thread.Sleep(500);
-            //            btnRacer1.RenderTransform = transform1;
-
-            //        }
-            //    });
-
-
-            //});
+            this.Dispatcher.BeginInvoke(() =>
+            {
+                racer1.UpdateX(btnRacer1, rnd.Next(4, 45));
+            });
+ 
+            //t1 = new Thread(UpdateCoordX);
             //t1.Start();
-
-            t1 = new Thread(UpdateCoordX);
-
-            t1.Start();
-
-
-            //transform1.X += rnd.Next(5, 45);
-            //btnRacer1.RenderTransform = transform1;
         }
         void StopClick(object sender, RoutedEventArgs e)
         {
-            t1.Abort();
+            //t1.Abort();
         }
 
-        void Move(TranslateTransform transform)
-        {
+        //void Move(TranslateTransform transform)
+        //{
 
-            transform.X += rnd.Next(5, 45);
-            //btnRacer1.RenderTransform = transform;
-            btnRacer1.Content = transform.X.ToString();
+        //    transform.X += rnd.Next(5, 45);
+        //    //btnRacer1.RenderTransform = transform;
+        //    btnRacer1.Content = transform.X.ToString();
 
-            Thread.Sleep(200);
+        //    Thread.Sleep(200);
 
 
-        }
+        //}
 
-        void UpdateCoordX()
-        {
-            //TranslateTransform transform = new TranslateTransform();
-            this.Dispatcher.BeginInvoke( new Action(()=>
-            {
-                while(transform1.X<=300)
-                {
-                    Move(transform1);
-                    Console.WriteLine(transform1.X);
-                }
+        //void UpdateCoordX()
+        //{
+        //    //TranslateTransform transform = new TranslateTransform();
+        //    this.Dispatcher.BeginInvoke( new Action(()=>
+        //    {
+        //        while(transform1.X<=300)
+        //        {
+        //            Move(transform1);
+        //            Console.WriteLine(transform1.X);
+        //        }
 
-            }));
-        }
+        //    }));
+        //}
+
+
     }
 }
--- Homework_L1/Homework_L1/Models/RacerModel.cs
+++ Homework_L1/Homework_L1/Models/RacerModel.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Homework_L1.Models
 {
-    class RacerModelcs
+    public class RacerModel
     {
+        Thread thread;
+
+        public event Action<Button, int> MoveByX;
+
+        TranslateTransform transform;
+
+        Button button;
+   
+        public int DeltaX;
+
+        const int STOP_LINE = 300;
+        public RacerModel(Button btn)
+        {
+            this.button = btn;
+            this.transform = new TranslateTransform();
+            thread = new Thread(GameIsOn);
+            thread.Start();
+       
+        }
+
+        public void GameIsOn()
+        {
+            UpdateX(button, DeltaX);
+        }
+
+        public void UpdateX(Button btn, int deltaX)
+        {
+            while(this.transform.X<=STOP_LINE)
+            {
+                MoveByX?.Invoke(btn, (int)transform.X);
+                transform.X += deltaX;
+                Thread.Sleep(200);
+
+            }
+        }
+
+
+
     }
 }
