9739962b8fb5ca8276e75eeb2ef446d86b9ac0d1
sergeybiophys <roven2hati@gmail.com>
2021-10-07 14:29:38 +0300

    dealt with delegates

--- Homework_L1/Homework_L1/MainWindow.xaml.cs
+++ Homework_L1/Homework_L1/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace Homework_L1
     public partial class MainWindow : Window
     {
 
+        RacerModel racer = new RacerModel();
+
         //Thread t1;
         //Thread t2;
         //Thread t3;
@@ -41,26 +43,77 @@ namespace Homework_L1
 
         RacerModel racer1;
 
+        Thread t1 = null;
+
+        public delegate void TestThis();
+        public TestThis delegateTestTest;
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
-           racer1  = new RacerModel(btnRacer1);
-            
-
+            racer1  = new RacerModel(btnRacer1, progress);
+            //racer.LoadingProgress += Racer_LoadingProgress;
+            progress.Maximum = 300;
+            //progress.Value = 50;
+           // delegateTestTest = new TestThis(TestTest);
+      
         }
 
+        private void Work()
+        {
+            for(int i = 0; i<=100; i++)
+            {
+                Thread.Sleep(200);
+                UpdateProgressBaar(i);
+            }
+        }
+
+        private void UpdateProgressBaar(int i)
+        {
+            Action action = () => { SetProgress(i); };
+            progress.Dispatcher.BeginInvoke(action);
+        }
+
+        private void SetProgress(int i)
+        {
+            progress.Value = i;
+        }
+        //private void Racer_LoadingProgress()
+        //{
+        //    Dispatcher.Invoke(new Action(() =>
+        //    {
+        //        racer.DoLoading2();
+        //    }));
+        //}
 
         void StartClick(object sender, RoutedEventArgs e)
         {
-            this.Dispatcher.BeginInvoke(() =>
-            {
-                racer1.UpdateX(btnRacer1, rnd.Next(4, 45));
-            });
- 
+            //racer.DeltaX = rnd.Next(5, 15);
+            //Racer_LoadingProgress();
+            //this.Dispatcher.BeginInvoke(() =>
+            //{
+            //    racer1.UpdateX(btnRacer1, rnd.Next(4, 45));
+            //});
+
             //t1 = new Thread(UpdateCoordX);
             //t1.Start();
+            //this.Title = progress.Value.ToString();
+
+            //this.Dispatcher.Invoke(delegateTestTest);
+            t1 = new Thread(Work);
+            t1.Start();
+        }
+
+        public void TestTest()
+        {
+            while(progress.Value<=300)
+            {
+                progress.Value += rnd.Next(5, 25);
+         
+                this.Title = progress.Value.ToString();
+                Thread.Sleep(500);
+            }
         }
         void StopClick(object sender, RoutedEventArgs e)
         {
--- Homework_L1/Homework_L1/Models/RacerModel.cs
+++ Homework_L1/Homework_L1/Models/RacerModel.cs
@@ -6,34 +6,44 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace Homework_L1.Models
 {
     public class RacerModel
     {
-        Thread thread;
+        public Thread thread;
 
         public event Action<Button, int> MoveByX;
+        public event Action LoadingProgress;
 
         TranslateTransform transform;
 
         Button button;
+        ProgressBar progressBar;
    
         public int DeltaX;
 
         const int STOP_LINE = 300;
-        public RacerModel(Button btn)
+        const int LOADING_VAL_PER_SEC = 10;
+        const int LOADING_DELAY = 500;
+
+        public RacerModel() { }
+        public RacerModel(Button btn, ProgressBar progress )
         {
             this.button = btn;
+            this.progressBar = progress;
             this.transform = new TranslateTransform();
             thread = new Thread(GameIsOn);
-            thread.Start();
+            //thread.Start();
        
         }
 
         public void GameIsOn()
         {
-            UpdateX(button, DeltaX);
+            DoLoading2();
+
+            //UpdateX(button, DeltaX);
         }
 
         public void UpdateX(Button btn, int deltaX)
@@ -45,6 +55,33 @@ namespace Homework_L1.Models
                 Thread.Sleep(200);
 
             }
+        }
+
+        //public void DoLoading()
+        //{
+        //    int loadingProgress = 0;
+
+        //    while (loadingProgress <= STOP_LINE)
+        //    {
+        //        LoadingProgress?.Invoke(loadingProgress);
+        //        loadingProgress += DeltaX;
+        //        Thread.Sleep(LOADING_DELAY);
+        //    }
+        //}
+
+        public void DoLoading2()
+        {
+            //int loadingProgress = 0;
+ 
+                while (progressBar.Value <= 300)
+                {
+                    LoadingProgress?.Invoke();
+                    progressBar.Value += DeltaX;
+
+                    Thread.Sleep(LOADING_DELAY);
+                }
+         
+
         }
 
 
